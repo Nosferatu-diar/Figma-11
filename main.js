9a4667db85073c69b4dc09cc6aa99158f62b6cc1
Berserk <dilmuradovid@gmail.com>
2024-12-18 14:12:46 +0500

    11-Figma

---
+++ main.js
@@ -0,0 +1,59 @@
+// Ex ---1---
+
+// function arrayMadness(a, b) {
+// 	const res1 = a.reduce((sum, num) => sum + num ** 2, 0)
+// 	const res2 = b.reduce((sum, num) => sum + num ** 3, 0)
+// 	return res1 > res2
+// }
+// console.log(arrayMadness([4, 5, 6], [1, 2, 3]))
+
+// Ex ---2---
+
+// function printArray(array){
+//     return array.join(",")
+//   }
+// let arr = ["h","o","l","a"]
+// console.log(printArray(arr));
+
+// Ex ---3---
+
+// var findAverage = function (nums) {
+// 	let sum = 0
+// 	for (let i = 0; i < nums.length; ++i) {
+// 		sum += nums[i]
+// 	}
+// 	return sum / nums.length
+// }
+
+// console.log(findAverage([1, 3, 5, 7]));
+
+// Ex ---4---
+
+// function stringToArray(string){
+//     return string.split(" ")
+// }
+// console.log(stringToArray("I love arrays they are my favorite"));
+
+// Ex ---5---
+
+// function getAverage(marks) {
+// 	let sum = 0
+// 	for (let i = 0; i < marks.length; i++) {
+// 		sum += marks[i]
+// 	}
+// 	return Math.floor(sum / marks.length)
+// }
+// console.log(getAverage([5, 3, 4, 2]))
+
+// Ex ---6---
+
+// function findSmallestInt(arr) {
+//     let min = arr[0]
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if(min > arr[i]){
+//             min = arr[i]
+//         }
+// 	}
+// 	return min
+// }
+// console.log(findSmallestInt([34, 15, 88, 2]));
